2ca42568ffe089afc14b77b78b07afe97f26485b
Agustin Arce <59893355+aarcex3@users.noreply.github.com>
2024-07-18 19:43:37 -0400

    Initial Commit

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,19 +2,19 @@
 
 const CONFIG = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    username: 'aarcex3', // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '/gitprofile/',
+  base: '/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
       header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      mode: 'manual', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
         limit: 8, // How many projects to display.
@@ -25,41 +25,24 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: [
+          'aarcex3/mygpo-clone',
+          'aarcex3/magic-wormhole-microservice',
+          'aarcex3/tweeter-clone-rails',
+        ], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
-    external: {
-      header: 'My Projects',
-      // To hide the `External Projects` section, keep it empty.
-      projects: [
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-      ],
-    },
+    external: {},
   },
   seo: {
-    title: 'Portfolio of Ariful Alam',
+    title: 'Portfolio of Agustin Arce',
     description: '',
     imageURL: '',
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: 'agustin-arce',
+    twitter: '',
+    mastodon: '',
     researchGate: '',
     facebook: '',
     instagram: '',
@@ -69,98 +52,129 @@ const CONFIG = {
     udemy: '',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: '',
+    dev: '',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://www.arifszn.com',
+    website: '',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'agustinarceprincich@gmail.com',
   },
   resume: {
-    fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+    fileUrl: '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
+    'Python',
     'JavaScript',
-    'React.js',
     'Node.js',
     'Nest.js',
     'MySQL',
     'PostgreSQL',
     'Git',
     'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'HTML/CSS',
+    'Linux',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+      company: 'Excelsis',
+      position: 'Data  Analyst Intern',
+      from: 'March 2024',
+      to: 'July 2024',
+      companyLink: 'https://www.excelsis.com.py',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Andreas STIHL AG & Co.KG',
+      position: 'Data Engineer Intern',
+      from: 'March 2023',
+      to: 'September 2023',
+      companyLink: 'https://www.stihl.de/de',
+    },
+    {
+      company: 'Aliado Movil',
+      position: 'Software Developer Intern',
+      from: 'July 2022',
+      to: 'August 2022',
+      companyLink: 'http://www.aliadomovil.com',
     },
   ],
   certifications: [
     {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com',
+      name: 'FastAPI - The Complete Course 2024 (Beginner + Advanced)',
+      body: 'Dive in and learn FastAPI from scratch! Learn FastAPI, RESTful APIs using Python, SQLAlchemy, OAuth, JWT and way more!',
+      year: 'July 2024',
+      link: 'https://www.udemy.com/certificate/UC-efd07488-2eb0-497d-a3e2-702dd4a5714c/',
+    },
+    {
+      name: 'Back End Development and APIs',
+      body: "In the Back End Development and APIs Certification, you'll learn how to write back end apps with Node.js and npm. You'll also build web applications with the Express framework, and build a People Finder microservice with MongoDB and the Mongoose library.",
+      year: 'April 2024',
+      link: 'https://www.freecodecamp.org/certification/czisa/back-end-development-and-apis',
+    },
+    {
+      name: 'Foundational C# with Microsoft',
+      body: 'This course offers a comprehensive introduction to C# programming, covering its core concepts, syntax, and practical application in software development.',
+      year: 'April 2024',
+      link: 'https://www.freecodecamp.org/certification/czisa/back-end-development-and-apis',
+    },
+    {
+      name: 'Python',
+      body: 'Learn the most important language for data science.',
+      year: 'March 2024',
+      link: 'https://www.kaggle.com/learn/certification/agustinarce003/python',
+    },
+    {
+      name: 'Intro to Programming',
+      body: 'Get started with Python, if you have no coding experience.',
+      year: 'July 2023',
+      link: '',
+    },
+    {
+      name: 'Git Essential Training: The Basics',
+      body: 'Learn git',
+      year: 'Nov 2022',
+      link: '',
+    },
+    {
+      name: 'Unit Testing and Test Driven Development in Python',
+      body: 'Learn TDD in python',
+      year: 'Nov 2022',
+      link: '',
+    },
+    {
+      name: 'Flask Framework Complete Course For Beginners',
+      body: 'Beginner to Expert Python And Flask. Learn Python And Flask Course for Beginner Python Developer to Create Applications',
+      year: 'May 2022',
+      link: 'https://www.udemy.com/certificate/UC-048b2fec-6c3f-4607-a24a-00a82b0849db/',
+    },
+    {
+      name: 'JavaScript Algorithms and Data Structures',
+      body: "While HTML and CSS control the content and styling of a page, JavaScript is used to make it interactive. In the JavaScript Algorithm and Data Structures Certification, you'll learn the fundamentals of JavaScript including variables, arrays, objects, loops, and functions.",
+      year: 'March 2021',
+      link: 'https://www.freecodecamp.org/certification/czisa/javascript-algorithms-and-data-structures',
     },
   ],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'Universidad Paraguayo Alemana',
+      degree: 'BSc Business Informatic',
+      from: '2020',
+      to: '2023',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Aalen University',
+      degree: 'BSc Business Informatic',
+      from: '2022',
+      to: '2023',
     },
   ],
-  publications: [
-    {
-      title: 'Publication Title',
-      conferenceName: '',
-      journalName: 'Journal Name',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-      title: 'Publication Title',
-      conferenceName: 'Conference Name',
-      journalName: '',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-  ],
+  publications: [],
   // Display articles from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
+    source: '', // medium | dev
+    username: '', // to hide blog section, keep it empty
+    limit: 0, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
