8235b28027cbe3ddce3b2a767828790beb19d907
Agustin Arce <agustinarceprincich@gmail.com>
2024-07-18 20:33:11 -0400

    Update gitprofile

--- src/components/gitprofile.tsx
+++ src/components/gitprofile.tsx
@@ -1,6 +1,11 @@
-import { useCallback, useEffect, useState } from 'react';
+/* eslint-disable prettier/prettier */
 import axios, { AxiosError } from 'axios';
 import { formatDistance } from 'date-fns';
+import { useCallback, useEffect, useState } from 'react';
+import { HelmetProvider } from 'react-helmet-async';
+import '../assets/index.css';
+import { BG_COLOR } from '../constants';
+import { DEFAULT_THEMES } from '../constants/default-themes';
 import {
   CustomError,
   GENERIC_ERROR,
@@ -8,28 +13,24 @@ import {
   INVALID_GITHUB_USERNAME_ERROR,
   setTooManyRequestError,
 } from '../constants/errors';
-import { HelmetProvider } from 'react-helmet-async';
-import '../assets/index.css';
-import { getInitialTheme, getSanitizedConfig, setupHotjar } from '../utils';
-import { SanitizedConfig } from '../interfaces/sanitized-config';
-import ErrorPage from './error-page';
-import HeadTagEditor from './head-tag-editor';
-import { DEFAULT_THEMES } from '../constants/default-themes';
-import ThemeChanger from './theme-changer';
-import { BG_COLOR } from '../constants';
-import AvatarCard from './avatar-card';
-import { Profile } from '../interfaces/profile';
-import DetailsCard from './details-card';
-import SkillCard from './skill-card';
-import ExperienceCard from './experience-card';
-import EducationCard from './education-card';
-import CertificationCard from './certification-card';
 import { GithubProject } from '../interfaces/github-project';
-import GithubProjectCard from './github-project-card';
-import ExternalProjectCard from './external-project-card';
+import { Profile } from '../interfaces/profile';
+import { SanitizedConfig } from '../interfaces/sanitized-config';
+import { getInitialTheme, getSanitizedConfig, setupHotjar } from '../utils';
+import AvatarCard from './avatar-card';
 import BlogCard from './blog-card';
+import CertificationCard from './certification-card';
+import DetailsCard from './details-card';
+import EducationCard from './education-card';
+import ErrorPage from './error-page';
+import ExperienceCard from './experience-card';
+import ExternalProjectCard from './external-project-card';
 import Footer from './footer';
+import GithubProjectCard from './github-project-card';
+import HeadTagEditor from './head-tag-editor';
 import PublicationCard from './publication-card';
+import SkillCard from './skill-card';
+import ThemeChanger from './theme-changer';
 
 /**
  * Renders the GitProfile component.
@@ -224,6 +225,12 @@ const GitProfile = ({ config }: { config: Config }) => {
                         skills={sanitizedConfig.skills}
                       />
                     )}
+                    {sanitizedConfig.educations.length !== 0 && (
+                      <EducationCard
+                        loading={loading}
+                        educations={sanitizedConfig.educations}
+                      />
+                    )}
                     {sanitizedConfig.experiences.length !== 0 && (
                       <ExperienceCard
                         loading={loading}
@@ -234,12 +241,6 @@ const GitProfile = ({ config }: { config: Config }) => {
                       <CertificationCard
                         loading={loading}
                         certifications={sanitizedConfig.certifications}
-                      />
-                    )}
-                    {sanitizedConfig.educations.length !== 0 && (
-                      <EducationCard
-                        loading={loading}
-                        educations={sanitizedConfig.educations}
                       />
                     )}
                   </div>
@@ -264,15 +265,15 @@ const GitProfile = ({ config }: { config: Config }) => {
                     )}
                     {sanitizedConfig.projects.external.projects.length !==
                       0 && (
-                      <ExternalProjectCard
-                        loading={loading}
-                        header={sanitizedConfig.projects.external.header}
-                        externalProjects={
-                          sanitizedConfig.projects.external.projects
-                        }
-                        googleAnalyticId={sanitizedConfig.googleAnalytics.id}
-                      />
-                    )}
+                        <ExternalProjectCard
+                          loading={loading}
+                          header={sanitizedConfig.projects.external.header}
+                          externalProjects={
+                            sanitizedConfig.projects.external.projects
+                          }
+                          googleAnalyticId={sanitizedConfig.googleAnalytics.id}
+                        />
+                      )}
                     {sanitizedConfig.blog.display && (
                       <BlogCard
                         loading={loading}
